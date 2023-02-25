c65f81339ed3000a56cfd26133abf298c90ebf7c
NeangThary <100552022+Neangthary@users.noreply.github.com>
2023-02-25 17:53:22 +0700

    sum two number
    sum two number user input

---
+++ sum.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream> 
+using namespace std;
+
+//write a program cpp to sum two number input by user
+
+int main () {
+    double num1, num2, result;
+    cout << "Enter first number: " <<endl;
+    cin >> num1;
+    cout << "Enter second number: " <<endl;
+    cin >> num2;
+    result = num1 + num2;
+
+    cout << num1 << " + " << num2 << " = " << result;
+    return 0;
+}
+
+
+
+
