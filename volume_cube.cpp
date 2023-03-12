8ae20161eab9a1d760ab47a037908d396c716039
NeangThary <100552022+Neangthary@users.noreply.github.com>
2023-03-12 08:04:11 +0700

    volume of cube

---
+++ volume_cube.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// Write a program to find the volume of cube
+
+int main()
+{
+    double a, vcube;
+    cout << "Enter the value of the connerz: ";
+    cin >> a;
+    vcube = a * a * a;
+    cout << "The volume of cube is: " << vcube << endl;
+}
